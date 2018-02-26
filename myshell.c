1ba3c8c4906c2e491948519fdab59b3da18c9787
Anthony <ajsouza88@gmail.com>
2018-02-26 00:33:42 -0800

    Add files via upload

---
+++ myshell.c
@@ -0,0 +1,30 @@
+/****************************************************************
+ * Name        :                                                *
+ * Class       : CSC 415                                        *
+ * Date  	   :                                                *
+ * Description :  Writting a simple bash shell program          *
+ * 	        	  that will execute simple commands. The main   *
+ *                goal of the assignment is working with        *
+ *                fork, pipes and exec system calls.            *
+ ****************************************************************/
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFFERSIZE 256
+#define PROMPT "myShell >> "
+#define PROMPTSIZE sizeof(PROMPT)
+#define ARGVMAX 64
+#define PIPECNTMAX 10
+
+int main(int* argc, char** argv)
+{
+
+    
+return 0;
+}
